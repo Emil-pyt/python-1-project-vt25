94f2b875bad59b210534f5c2d484afdb1a18d6a6
Emil Högberg <Emil.hogberg.e@skola.karlskrona.se>
2025-01-18 11:16:26 +0100

    Review of printouts renaming of rooms formating prints Back, east, west, south instead of L, R, Fwd

--- python_1_project_vt25.py
+++ python_1_project_vt25.py
@@ -18,15 +18,16 @@ Inventory = []
 #import tkinter as tk
 
 
-def roomDialog(parRoom, roomName, roomFwd = 0, roomRight = 0, roomLeft = 0):
-    print("You are now in room " + roomName)  
+def roomDialog(parRoom, roomName, roomSouth = 0, roomWest = 0, roomEast = 0):
+    if roomName != pantry:
+        print("You are now in the " + roomName)  
     print("These are your options here: ")
-    if roomFwd != 0:
-       print(f"{'forward (w)': >42}")
-    if roomLeft != 0:
-       print(f"{'left (a)': >42}")
-    if roomRight != 0:
-        print(f"{'right (d)': >42}")
+    if roomSouth != 0:
+       print(f"{'South (w)': >42}")
+    if roomEast != 0:
+       print(f"{'East (a)': >42}")
+    if roomWest != 0:
+        print(f"{'West (d)': >42}")
     if roomName != "room 1":
         print(f"{'back (s)': >42}")
     print(f"{'inventory (i)': >42}")
@@ -35,21 +36,21 @@ def roomDialog(parRoom, roomName, roomFwd = 0, roomRight = 0, roomLeft = 0):
     usrInput=input("Where do you want to go? ")
     match usrInput:
         case "a":
-            if roomLeft != 0:
+            if roomEast != 0:
                 lastRoom = parRoom
-                room = roomLeft
+                room = roomEast
             else: 
                 print("You didn't reed? Did you?")
         case "w":
-            if roomFwd != 0:
+            if roomSouth != 0:
                 lastRoom = parRoom
-                room = roomFwd
+                room = roomSouth
             else: 
                 print("You didn't reed? Did you?")
         case "d":
-            if roomRight != 0:
+            if roomWest != 0:
                 lastRoom = parRoom
-                room = roomRight
+                room = roomWest
             else: 
                 print("You didn't reed? Did you?")
         case "s": # going back
@@ -61,21 +62,28 @@ def roomDialog(parRoom, roomName, roomFwd = 0, roomRight = 0, roomLeft = 0):
         case _:
             print("The command does not exist, try again ")
     return
+def intro():
+    print("Your goal is to collect material in the different rooms to fight the giant that is blocking the door so you can't escape. Good luck!")
+    print("To walk you use the commands:    W - South, A - East, D - West, S - back") 
+def pantry():
+    print("Woah! You have just woken up. The room you're in is dark.")
+    print("You try to get out but the door is locked, so you feel around in the room trying to find something heavy to smash the door.") 
+    print("You are lucky this time, the room seems to have a lot of food and kitchen ware so you can quickly find a mortal to smash the door") 
 
 
 
 
 
 
-
- 
-print("Welcome! You are stuck in a abondend house. Your goal is to collect material in the different rooms to fight the giant that is blocking the door so you can't escape. Good luck!")
-print("To walk you use the commands:    W - Forward, A - left, D - right, S - backwards")
+  
+intro()
 while 1:
     if room == 1:
-        roomDialog(room, roomName = "room 1", roomFwd = 2)
+        if lastRoom == 1:
+            pantry()
+        roomDialog(room, roomName = "Pantry", roomSouth = 2)
     if room == 2: 
-        roomDialog(room, roomName = "Kitchen", roomFwd = 6, roomRight = 3, roomLeft = 4)
+        roomDialog(room, roomName = "livingroom", roomSouth = 6, roomWest = 3, roomEast = 4)
        
     if room == 3: 
         print("You are in room 3")
@@ -85,15 +93,18 @@ while 1:
         print("The third digit is the number of sides on a rectangle minus the number of sides on a triangle.")
         print("The fourth digit is the number of continents that start with the letter A.")
         print("What is the code? You could maybe need it in the future")
-        roomDialog(room, roomName = "3, the livingroom", roomLeft = 5) 
+        roomDialog(room, roomName = "3, the livingroom", roomEast = 5) 
     if room == 4:
-        roomDialog(room, roomName = "4", roomRight = 7)
+        roomDialog(room, roomName = "4", roomSouth = 7, roomWest = 2)
     if room == 5:
-        roomDialog(room, roomName = "5, the basement")
+        roomDialog(room, roomName = "5, basement")
     if room == 6:
-        roomDialog(room, roomName = "6", roomLeft = 7)      
+        roomDialog(room, roomName = "the kitchen", roomEast = 7)      
     if room == 7:
-        roomDialog(room, roomName = "7, the final room", roomRight = 7)
+        roomDialog(room, roomName = "7, the final room", roomWest = 7)
 
         #rum 4
-        # d p om till riktningar - v derstr ck
+        # dop om till riktningar - vaderstrack
+        #rand for om man skriver fel
+        # fy den att inte printa pantry f rsta g ngen n r def k rs
+        # rum 2  r k k, rum 3 sovrum, rum 5 k llare, 
