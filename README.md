ddc51226ee81c7685015e75f30ba86730ba117df
Emil Högberg <Emil.hogberg.e@skola.karlskrona.se>
2025-01-31 20:45:23 +0100

    Added some more comments

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # python-1-project-vt25
+This is a python programming assignment in a Python introduction course. The task is to take inspiration from the old game Zork (https://www.youtube.com/watch?v=xzUagi41Wo0).
+The solution is done object oriented with a class called room. 
+The idea of this game is to under two min collect specified items in a specified order otherwise you will die if the time runs out.
+To gain more time you can get a health smoothie but there is only four of them to be collected.
+If you try to get out of the house with the wrong items you will get pushed back to the house and the items you have collected will be put in random rooms.  
+
--- python_1_project_vt25.py
+++ python_1_project_vt25.py
@@ -8,6 +8,7 @@ import os
 import time
 from random import randint, random
 
+#Settings
 MAX_HP_CHARACTERS = 20 #number of printed stars when full hp
 MAX_HP_SECONDS = 120 # seconds corresponding to MAX_HP_CHARACTERS
 MAX_NR_SMOOTHIES = 4
@@ -15,19 +16,20 @@ HP_BOOSTER = "Health smoothie"
 SMOOTHIE_EXTRA_SEC = 20
 REQUIRED_LIST = ["Alarm code", "Bread", "Torch"]
 
-
+# print banner file
 def print_file(fn):
     f= open(fn,'r')
     print(''.join([line for line in f]))
 
 
 
- 
+# calculate hp from elapsed time and nr of taken smoothies
 def calc_hp( nr_of_chars, nr_of_sec, start_time_p, smoothie_time_p):
     elapsed_time = time.perf_counter() - start_time_p - smoothie_time_p #might get negative but it´s okay if many smoothies taken early
     hp = round(nr_of_chars * (1-elapsed_time/nr_of_sec))
     return hp
 
+# clears the screen 
 def clear_screen():  
     if os.name == 'nt': 
         _ = os.system('cls')  # For Windows
@@ -35,19 +37,19 @@ def clear_screen():
         _ = os.system('clear') # For macOS and Linux
 
 
-'''
-initialization
-creates room objects and add them to the "rooms" tuple
-also adds item to the rooms
-'''
+
+#initialization
+#creates room objects and add them to the "rooms" tuple
+#also adds item to the rooms
+
 nr_smoothies = MAX_NR_SMOOTHIES
 r0 = Room(name = "Pantry", room_id_n = 1)
 r0.add_item_list(["Canned jars", "Bread"])
 r1 = Room(name = "Kitchen", room_id_n = 5, room_id_s = 0, room_id_e=2, room_id_w=3)
-r1.add_item_list([HP_BOOSTER, "Broom"])
+r1.add_item_list( [HP_BOOSTER, "Broom"] )
 
 r2 = Room(name = "Bedroom", room_id_n = 4, room_id_w = 1)
-r2.add_into_item_list("Iron sword")
+r2.add_into_item_list("Ipad")
 
 r3 = Room(name = "Main Living room", room_id_n = 6, room_id_w = 1)
 r3.add_item_list(["Paper", "Pen", "Rope"])
@@ -77,7 +79,7 @@ while 1:
     usrInput=input(current_room.generate_room_str(rooms_in_house, print_err_msg, inventory, REQUIRED_LIST))
     print_err_msg = False
     match usrInput:
-        case "0"|"1"|"2"|"3":
+        case "0"|"1"|"2"|"3": #pick an item into inventory-list
             if int(usrInput) < len(current_room.item_list):  #if user pressed 0 make sure there are at least 1 item in the list
                 inventory.append( current_room.get_from_item_list(int(usrInput)) )
                 if HP_BOOSTER in inventory: #if we added HP_BOOSTER in inventory -> pop it and add extra seconds to hp calc
@@ -85,7 +87,7 @@ while 1:
                     smoothie_time += SMOOTHIE_EXTRA_SEC
             else:
                 print_err_msg = True
-        case "a"|"d"|"w"|"s":
+        case "a"|"d"|"w"|"s": #enter neighbouring room
             new_room_id = current_room.get_room_id(usrInput)
             if new_room_id  != -1:
                 last_room = current_room
@@ -102,6 +104,7 @@ while 1:
     
     
     hp = calc_hp(MAX_HP_CHARACTERS, MAX_HP_SECONDS, start_time, smoothie_time)
+    # check if game over
     if hp >= 0:
         print_file('banner_zork.txt')
         print("Your health is:  " + "*" * hp + "-" * (20-(hp)))
@@ -128,14 +131,3 @@ while 1:
                 rooms_in_house[room_rand_number].add_into_item_list(item)  
             inventory = []
             current_room = last_room #forces the usr back into the house
-
-'''             
-   
-        #riddle in room 3
-        print("You have found a paper behind the door. It seems like it's a riddle!? You take up the paper and read the riddle")
-        print("The first digit is the number of weeks in two months.")
-        print("The second digit is the number of months that have 28 days.")
-        print("The third digit is the number of sides on a rectangle minus the number of sides on a triangle.")
-        print("The fourth digit is the number of continents that start with the letter A.")
-        print("What is the code? You could maybe need it in the future")
-        '''
